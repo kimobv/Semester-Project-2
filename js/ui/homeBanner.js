207050c8032aa63f09c1302c7e5685d3e574f6ed
Kim Oliver B Væring <kimobv@icloud.com>
2022-12-11 14:09:32 -0800

    Update homeBanner.js

--- js/ui/homeBanner.js
+++ js/ui/homeBanner.js
@@ -8,9 +8,9 @@ export async function homeBanner() {
 		const response = await fetch(heroBannerUrl);
 		const products = await response.json();
 		//heroBannerContainer.innerHTML = `<img src="${products.hero_banner.url}" class="flex-grow-1" alt="...">`;
-		heroBannerContainer.innerHTML = `<img src="https://res.cloudinary.com/kimobv/image/upload/v1670793068/hero.jpg" class="flex-grow-1" alt="...">`;
+		heroBannerContainer.innerHTML = `<img src="https://res.cloudinary.com/kimobv/image/upload/v1670793068/hero.jpg" class="banner flex-grow-1" alt="...">`;
 	} catch (error) {
 		//displayMessage('error', 'An error has occoured', '#hero');
-		heroBannerContainer.innerHTML = `<img src="https://res.cloudinary.com/kimobv/image/upload/v1670793068/hero.jpg" class="flex-grow-1" alt="...">`;
+		heroBannerContainer.innerHTML = `<img src="https://res.cloudinary.com/kimobv/image/upload/v1670793068/hero.jpg" class="banner flex-grow-1" alt="...">`;
 	}
 }
