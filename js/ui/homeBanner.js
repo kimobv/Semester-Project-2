a4cb3a67697e01165a38fdf9a5c54ef8d8896ab9
Kim Oliver B Væring <kimobv@icloud.com>
2022-12-11 14:08:02 -0800

    Update homeBanner.js

--- js/ui/homeBanner.js
+++ js/ui/homeBanner.js
@@ -11,6 +11,6 @@ export async function homeBanner() {
 		heroBannerContainer.innerHTML = `<img src="https://res.cloudinary.com/kimobv/image/upload/v1670793068/hero.jpg" class="flex-grow-1" alt="...">`;
 	} catch (error) {
 		//displayMessage('error', 'An error has occoured', '#hero');
-		heroBannerContainer.innerHTML = `<img src="https:///res-console.cloudinary.com/kimobv/thumbnails/transform/v1/image/upload/v1670796381/https:///res-console.cloudinary.com/kimobv/thumbnails/transform/v1/image/upload/Y19jcm9wLGhfMjY1Nix5XzY4Mg==/v1670796381/aGVybw==/template_primary" class="flex-grow-1" alt="...">`;
+		heroBannerContainer.innerHTML = `<img src="https://res.cloudinary.com/kimobv/image/upload/v1670793068/hero.jpg" class="flex-grow-1" alt="...">`;
 	}
 }
