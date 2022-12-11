ec4c0d817f8f2914b8a037bb310e6618526abcb3
Kim Oliver B Væring <kimobv@icloud.com>
2022-12-11 13:55:01 -0800

    Update api.js

--- js/settings/api.js
+++ js/settings/api.js
@@ -1,3 +1,4 @@
 export const baseUrl = 'https://sp2-strapi.herokuapp.com/';
 export const productUrl = 'https://sp2-strapi.herokuapp.com/products/';
-export const heroBannerUrl = 'https://sp2-strapi.herokuapp.com/home';
+//export const heroBannerUrl = 'https://sp2-strapi.herokuapp.com/home';
+export const heroBannerUrl = 'https://res.cloudinary.com/kimobv/image/upload/v1670793068/hero.jpg';
