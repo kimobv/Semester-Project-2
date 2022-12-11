60ffd40c02382184b89c46ee7365abef71a4070c
Kim Oliver B Væring <kimobv@icloud.com>
2022-12-11 14:31:56 -0800

    Update homeBanner.js

--- js/ui/homeBanner.js
+++ js/ui/homeBanner.js
@@ -7,7 +7,7 @@ export async function homeBanner() {
 	try {
 		const response = await fetch(heroBannerUrl);
 		const products = await response.json();
-		heroBannerContainer.innerHTML = `<img src="${products.hero_banner.url}" class="flex-grow-1" alt="...">`;
+		heroBannerContainer.innerHTML = `<img src="${heroBannerUrl}" class="flex-grow-1" alt="...">`;
 	} catch (error) {
 		displayMessage('error', 'An error has occoured', '#hero');
 	}
