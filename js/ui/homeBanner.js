a4a474229ca269efc2b6e56f9b1429c7939aca1b
Kim Oliver B Væring <kimobv@icloud.com>
2022-12-11 13:57:08 -0800

    Update homeBanner.js

--- js/ui/homeBanner.js
+++ js/ui/homeBanner.js
@@ -7,8 +7,8 @@ export async function homeBanner() {
 	try {
 		const response = await fetch(heroBannerUrl);
 		const products = await response.json();
-
-		heroBannerContainer.innerHTML = `<img src="${products.hero_banner.url}" class="flex-grow-1" alt="...">`;
+		//heroBannerContainer.innerHTML = `<img src="${products.hero_banner.url}" class="flex-grow-1" alt="...">`;
+		heroBannerContainer.innerHTML = `<img src="https://res.cloudinary.com/kimobv/image/upload/v1670793068/hero.jpg" class="flex-grow-1" alt="...">`;
 	} catch (error) {
 		displayMessage('error', 'An error has occoured', '#hero');
 	}
