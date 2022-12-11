1dd73013413956befcd1a4165a780392ef57be8f
Kim Oliver B Væring <kimobv@icloud.com>
2022-12-11 14:04:13 -0800

    Update homeBanner.js

--- js/ui/homeBanner.js
+++ js/ui/homeBanner.js
@@ -11,6 +11,6 @@ export async function homeBanner() {
 		heroBannerContainer.innerHTML = `<img src="https://res.cloudinary.com/kimobv/image/upload/v1670793068/hero.jpg" class="flex-grow-1" alt="...">`;
 	} catch (error) {
 		//displayMessage('error', 'An error has occoured', '#hero');
-		heroBannerContainer.innerHTML = `<img src="https://res.cloudinary.com/kimobv/image/upload/h_300,w_970/v1670793068/hero.jpg" class="flex-grow-1" alt="...">`;
+		heroBannerContainer.innerHTML = `<img src="https://res.cloudinary.com/kimobv/image/upload/c_fill,g_auto,h_300,w_970,w_0.5,y_0.18/v1670793068/hero.jpg" class="flex-grow-1" alt="...">`;
 	}
 }
